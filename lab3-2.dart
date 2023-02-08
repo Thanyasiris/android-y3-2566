c92edc2d959853e0f9b24d354f0fe991cdc379c8
PxQo <76105638+Thanyasiris@users.noreply.github.com>
2023-02-08 11:18:09 +0700

    Create lab3-2.dart
    not finish

---
+++ lab3-2.dart
@@ -0,0 +1,20 @@
+void main() {
+  List<String> char = ['a','b','c','d','e'];
+  List<int> freq = [3,5,9,11,19];
+  List<String> output=[];
+  for (int i = 0; i < char.length ; i++) {
+    int set= (freq[i]/5).floor();
+    output.add("${char[i]} :");
+    for(int j=0;j<set;j++){
+      if(j==set-1&&freq[i]%5==0){
+        output[i] += "-----";
+      }else{
+        output[i] += "-----|";
+      }
+    }
+    for(int k=0;k<freq[i]%5;k++){
+      output[i] += "-";
+    }
+    print(output[i]);
+  }
+}
