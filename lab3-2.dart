842c41ee1942b1dcc2297e1e59d9005013c59c40
PxQo <76105638+Thanyasiris@users.noreply.github.com>
2023-02-08 12:02:49 +0700

    Update lab3-2.dart
    nonono

--- lab3-2.dart
+++ lab3-2.dart
@@ -1,20 +1,31 @@
 void main() {
-  List<String> char = ['a','b','c','d','e'];
-  List<int> freq = [3,5,9,11,19];
-  List<String> output=[];
-  for (int i = 0; i < char.length ; i++) {
-    int set= (freq[i]/5).floor();
-    output.add("${char[i]} :");
-    for(int j=0;j<set;j++){
-      output[i] += "-----";
-      if(j!=set-1||freq[i]%5!=0){
-        output[i] += "|";
-      }
+  String str = "aaAAAcsh";
+  List<String> char = str.split('');
+  char.sort();
+  print(char);
+  for(int i=0;i<char.length;i++){
+    if(char[i].codeUnitAt(0)>=65&&char[i].codeUnitAt(0)<=90){
+      char.add(char[0]);
+      char.removeAt(0);
+    }else{
+      i=char.length;
     }
-    for(int k=0;k<freq[i]%5;k++){
-      output[i] += "-";
-    }
-    print(output[i]);
   }
+  print(char);
+//   List<int> freq = [3,5,9,11,19];
+//   List<String> output=[];
+//   for (int i = 0; i < char.length ; i++) {
+//     int set= (freq[i]/5).floor();
+//     output.add("${char[i]} :");
+//     for(int j=0;j<set;j++){
+//       output[i] += "-----";
+//       if(j!=set-1||freq[i]%5!=0){
+//         output[i] += "|";
+//       }
+//     }
+//     for(int k=0;k<freq[i]%5;k++){
+//       output[i] += "-";
+//     }
+//     print(output[i]);
+//   }
 }
-
